a43db77051270259c35776b361891eace59daf2a
Mohamed <abdi2k24@gmail.com>
2022-09-19 19:42:44 -0500

    added Var for password

--- Develop/script.js
+++ Develop/script.js
@@ -6,10 +6,18 @@ function writePassword() {
   var UppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var LowercaseCwercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var NumbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var SpecialcharacterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+  var SpecialcharacterArray = ["!","@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
   var returnArray = [];
   var inputArray = [];
+  
+  uppercaseArray [1]
+  
+    var numCharacter = prompt ("How many Characters do you want between 8 and 128?");
+    var numbers = confirm (" Would you like numbers in your password?");
+    var uppercases = confirm ("Would you like to include uppercases in your password?");
+    var lowercases = confirm ("Would yoou like to include owercases in your password?");
+    var Specialcharacters = confirm ("Would you like to include special characters in your password?");
   
   
  
